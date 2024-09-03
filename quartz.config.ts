1c6f608bbf4ea9bb039997c708f3e0ef1d9d5c60
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-09-03 15:29:24 +0100

    draft

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🗻 Offgrid8",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
