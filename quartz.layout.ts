20e0e82f79454f6636dccd142c3bffdc2cdbc5d7
offgrid88 <45432048+offgrid88@users.noreply.github.com>
2024-09-17 12:11:48 +0100

    edit graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,9 +36,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true, // whether to allow zooming in and out
         depth: 1, // how many hops of notes to display
         scale: 1.1, // default view scale
-        repelForce: 0.5, // how much nodes should repel each other
-        centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
+        repelForce: 0.9, // how much nodes should repel each other
+        centerForce: 0.2, // how much force to use when trying to center the nodes
+        linkDistance: 80, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
@@ -49,9 +49,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 0.5,
-        centerForce: 0.3,
-        linkDistance: 30,
+        repelForce: 0.9,
+        centerForce: 0.2,
+        linkDistance: 100,
         fontSize: 0.6,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
